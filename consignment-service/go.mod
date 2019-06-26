07bc372406916eddcb2310e3da25b9aff43e3b0f
Dmitry Bagdanov <dimbo_job@mail.ru>
2019-06-26 15:15:35 +0700

    migrate consignment-service to MongoDB

--- consignment-service/go.mod
+++ consignment-service/go.mod
@@ -4,9 +4,13 @@ go 1.12
 
 require (
 	github.com/dfbag7/shipy/vessel-service v0.0.0-20190625162358-6c11f96ceccf
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.3.1
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/micro/go-micro v1.7.0
-	github.com/micro/protobuf v0.0.0-20180321161605-ebd3be6d4fdb // indirect
+	github.com/tidwall/pretty v1.0.0 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.0.3
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
-	google.golang.org/grpc v1.21.1
 )
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	// Import the generated protobuf code
 	pb "github.com/dfbag7/shipy/consignment-service/proto/consignment"
@@ -13,30 +14,11 @@ import (
 	"github.com/micro/go-micro"
 )
 
-type repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
+const (
+	defaultHost = "datastore:27017"
+)
 
-// Repository - Dummy repository, this simulates the use of a datastore 
-// of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
-	consignments []*pb.Consignment
-}
-
-// Create a new consignment
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-
-	return consignment, nil
-}
-
-// GetAll consignments
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
+/*
 // Service should implement all of the methods to satisfy the service
 // we defined in our protobuf definition. You can check the interface
 // in the generated code itself for the exact method signatures etc
@@ -68,30 +50,31 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 	req.VesselId = vesselResponse.Vessel.Id
 
 	// Save our consignment
-	consignment, err := s.repo.Create(req)
+	err := s.repo.Create(req)
 	if err != nil {
 		return err
 	}
 
 	res.Created = true
-	res.Consignment = consignment
+	res.Consignment = req
 
 	return nil
 }
 
 // GetConsignments -
 func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
+	consignments, err := s.repo.GetAll()
+	if err != nil {
+		return err
+	}
 
 	res.Consignments = consignments
 
 	return nil
 }
+*/
 
 func main() {
-
-	repo := &Repository{}
-
 	// Set-up micro service
 	srv := micro.NewService(
 		// This name must match the package name given in your protobuf definition
@@ -102,10 +85,25 @@ func main() {
 	// Init will parse the command line flags.
 	srv.Init()
 
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
+
+	client, err := CreateClient(uri)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	consignmentCollection := client.Database("shipy").Collection("consignments")
+
+	repository := &MongoRepository{consignmentCollection}
 	vesselClient := vesselProto.NewVesselServiceClient("shipy.vessel.service", srv.Client())
+	h := &handler{repository, vesselClient}
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
+	pb.RegisterShippingServiceHandler(srv.Server(), h)
 
 	// Run the server
 	if err := srv.Run(); err != nil {
